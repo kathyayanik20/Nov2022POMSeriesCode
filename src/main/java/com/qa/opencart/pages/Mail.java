a3a03df92e33ce0cafbf091953d3df824ffa0ec4
Kathyayani <kathyayanik2013@gmail.com>
2023-05-11 09:29:14 -0500

    Mail Naveen

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+	
+	public void sendMail(String[] args) {
+		
+		System.out.println(" Mail Sent ----Naveen");
+	}
+
+    public void getMail(String[] args) {
+		
+		System.out.println(" get Mail ----Naveen");
+	}
+
+}
